2ecf5e2ae52cbc3ce170ca0b815f29933cc39ed4
aramli <78140980+aramli@users.noreply.github.com>
2025-12-14 14:29:50 +0700

    Update Tugas 5 Pertemuan 13(ValidasiNilai).py

--- Tugas 5 Pertemuan 13(ValidasiNilai).py
+++ Tugas 5 Pertemuan 13(ValidasiNilai).py
@@ -1,17 +1,18 @@
 nilai = [80, 90, 'A', 70, 100, 'B']
 
 total = 0
-jumlah_data = 0
+jum_data = 0
 
-for n in nilai:
+for i in nilai:
     try:
-        total += n
-        jumlah_data += 1
+        total += i
+        jum_data += 1
     except TypeError:
-        print(f"Data '{n}' bukan angka, dilewati.")
+        print(f"Data '{i}' bukan angka, dilewati.")
 
-if jumlah_data > 0:
-    rata_rata = total / jumlah_data
-    print("Rata-rata nilai:", rata_rata)
+if jum_data > 0:
+    avg = total / jum_data
+    print("Rata-rata nilai:", avg)
 else:
     print("Tidak ada data nilai yang valid.")
+
